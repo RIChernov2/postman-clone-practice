606f51ca2ef9e722d62f933f835f48b0b04c0b43
RIChernov2 <r.i.chernov@mail.ru>
2024-12-04 03:44:46 +0300

    init

--- PostmanCloneUI,WinForm/DashBoard.cs
+++ PostmanCloneUI,WinForm/DashBoard.cs
@@ -1,8 +1,8 @@
 namespace PostmanCloneUI_WinForm
 {
-    public partial class Form1 : Form
+    public partial class DashBoard : Form
     {
-        public Form1()
+        public DashBoard()
         {
             InitializeComponent();
         }
